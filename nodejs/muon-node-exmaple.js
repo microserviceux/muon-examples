a361aad0b48e113975a6f4b4e5ee3f65e2d39622
David Dawson <david.dawson@simplicityitself.com>
2015-07-07 16:43:48 +0100

    Updates

--- nodejs/muon-node-exmaple.js
+++ nodejs/muon-node-exmaple.js
@@ -8,19 +8,9 @@ muon.addTransport(amqp);
 
 
 setTimeout(function() {
-    
 
     muon.resource.query("muon://echo/echo", function(event, payload) {
-            try {
-                if (event.Status == "404") {
-                    logger.error("Service returned 404 when accessing " + args[0]);
-                } else {
-                    console.dir(payload);
-                }
-            } catch (e) {
-                logger.error("Failed to render the response", e);
-            }
-            process.exit(0);
+            console.log(payload);
         });
     
 },3500);
